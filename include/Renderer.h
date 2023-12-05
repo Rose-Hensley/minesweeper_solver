8b4989bb34792d89266f86b913de367828334554
Rosanna Hensley <64337845+Rose-Hensley@users.noreply.github.com>
2023-12-04 19:36:19 -0500

    Add files via upload

--- include/Renderer.h
+++ include/Renderer.h
@@ -1,7 +1,24 @@
 #ifndef MINESWEEPER_RENDERER_H
 #define MINESWEEPER_RENDERER_H
 
-void render_init(int wd, int ht);
-void render_two();
+class Renderer {
+private:
+    int block_size;
+
+    void draw_horz();
+
+    void draw_vert();
+
+    std::tuple<int, int> get_pixel_pos(int x, int y);
+
+public:
+    Renderer(int grid_wd, int grid_ht);
+
+    void render_init(int wd, int ht);
+
+    void write_number(int x, int y, int n);
+
+};
+
 
 #endif //MINESWEEPER_RENDERER_H
