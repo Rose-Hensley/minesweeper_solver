ddd52202ba3d2a5abd6b465a5d4a772a96a22fd8
Rosanna Hensley <64337845+Rose-Hensley@users.noreply.github.com>
2023-12-04 19:36:04 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 #include "Renderer.h"
 
 int main(int argc, char* argv[]) {
-    int wd, ht = 10;
-    render_init(wd, ht);
-    render_two();
+    int wd = 8, ht = 10;
+    Renderer r(wd, ht);
+    r.render_init(wd, ht);
 
     return 0;
 }
