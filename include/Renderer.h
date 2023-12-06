c2232877b37cded0190148dda21e901412d9451a
Rosanna Hensley <64337845+Rose-Hensley@users.noreply.github.com>
2023-12-06 18:16:52 -0500

    Add files via upload

--- include/Renderer.h
+++ include/Renderer.h
@@ -5,18 +5,36 @@ class Renderer {
 private:
     int block_size;
 
+    // Grid width, or how many blocks across
+    int wd;
+
+    // Grid height, or how many blocks tall
+    int ht;
+
     void draw_horz();
 
     void draw_vert();
 
     std::tuple<int, int> get_pixel_pos(int x, int y);
 
+    std::tuple<int, int> get_write_pos(int x, int y);
+
+    std::string get_number_color(int n);
+
 public:
     Renderer(int grid_wd, int grid_ht);
 
-    void render_init(int wd, int ht);
+    void render_init();
 
-    void write_number(int x, int y, int n);
+    void draw_number(int x, int y, int n);
+
+    void draw_bomb(int x, int y);
+
+    void draw_flag(int x, int y);
+
+    void end_on_click();
+
+    void test();
 
 };
 
