7d3a4ac54e8b9bda6cf0c2edfddb678a8e160bfa
Rosanna Hensley <64337845+Rose-Hensley@users.noreply.github.com>
2023-12-07 13:51:20 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
-#include "Renderer.h"
+#include <random>
+#include "Controller.h"
 #include "argh.h"
 
 int main(int argc, char* argv[]) {
-    int wd = 30, ht = 16;
-    Renderer r(wd, ht);
-    r.render_init();
-    r.draw_bomb(0, 0);
-    r.draw_flag(1, 0);
-    r.draw_number(wd - 1, ht - 1, 3);
-    r.end_on_click();
+    srand(time(0));
+    int wd = 9, ht = 9, bs = 1;
+    Controller c(wd,ht,bs);
+    c.solve_dumb();
     return 0;
 }
