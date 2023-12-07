69b8b74c628770b890004b645fb2c91a1f877133
Rosanna Hensley <64337845+Rose-Hensley@users.noreply.github.com>
2023-12-07 11:11:21 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include "Renderer.h"
+#include "argh.h"
 
 int main(int argc, char* argv[]) {
     int wd = 30, ht = 16;
     Renderer r(wd, ht);
     r.render_init();
-    //r.test();
+    r.draw_bomb(0, 0);
+    r.draw_flag(1, 0);
+    r.draw_number(wd - 1, ht - 1, 3);
     r.end_on_click();
     return 0;
 }
